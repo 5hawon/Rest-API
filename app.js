61e83e376953c18c4387fee99677da3723301671
5hawon <sshawon993@gmail.com>
2024-03-24 03:02:33 +0600

    Initial connection

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const products_routes = require('./routes/products')
+
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+    res.send("Hi I am live");
+})
+
+//middleware routes
+app.use("/api/products",products_routes);
+
+const start = async()=>{
+    try {
+        app.listen(PORT,()=>{
+            console.log("listening on port "+PORT);
+        })
+        
+    } catch (error) {
+        console.log(error);
+        
+    }
+}
+
+start();
